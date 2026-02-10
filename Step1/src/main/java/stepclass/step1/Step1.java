c0d7b12710893749ad33940eb9c7408b49ee359b
Cyberbee-pro <Cyberbeepro@gmail.com>
2026-02-10 14:44:39 +0530

    initial commit

---
+++ Step1/src/main/java/stepclass/step1/Step1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package stepclass.step1;
+
+/**
+ *
+ * @author Cyberbee
+ */
+public class Step1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
